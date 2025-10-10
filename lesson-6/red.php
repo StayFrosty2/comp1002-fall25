970e70b2e68a8d70e5d90a9abee07c7efb215030
Claire <cameron.liam.yon@gmail.com>
2025-10-10 15:55:26 +0000

    Finished all pages, added navigation

--- lesson-6/red.php
+++ lesson-6/red.php
@@ -15,13 +15,18 @@
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 			<!-- Global site navigation -->
+			<!-- Claire Comment: Did this on the line below instead -->
+<!-- STEP 5a: We are now marooned on this page with no way back to the home page except the 'Back' button - build an LI and an A element above pointing back to the index.php page -->
 			<nav>
 				<ul>
-<!-- STEP 5a: We are now marooned on this page with no way back to the home page except the 'Back' button - build an LI and an A element above pointing back to the index.php page -->
-
-<!-- STEP 5b: Upload this changed file, and try it in the browser - then go back to index.php for STEP 6 -->
+					<li><a href="index.php">Home</a></li>
+					<li><a href="red.php">Red Blocks</a></li>
+					<li><a href="blue.php">Blue Blocks</a></li>
+					<li><a href="yellow.php">Yellow Blocks</a></li>
 				</ul>
 			</nav>
+<!-- STEP 5b: Upload this changed file, and try it in the browser - then go back to index.php for STEP 6 -->
+
 		</header>
 		<!-- Page-level main content -->
 		<main>
